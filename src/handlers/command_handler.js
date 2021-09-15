fe308cf73d0ec7548ad24034b5ca75e5ec230d9c
Austin <yo861015@gmail.com>
2021-09-15 10:55:14 +0800

    chore: update command handler

--- src/handlers/command_handler.js
+++ src/handlers/command_handler.js
@@ -1,18 +1,13 @@
 const fs = require('fs');
-//global data
-const commandList = [];
 
-const event_handler = (client, Discord) => {
+module.exports = (client) => {
   const commandFiles = fs.readdirSync('./src/commands').filter((file) => file.endsWith('.js'));
   for (const file of commandFiles) {
     const command = require(`../commands/${file}`);
     if (command.name) {
       client.commands.set(command.name, command);
-      commandList.push(command.name);
     } else {
       continue;
     }
   }
 };
-
-module.exports = event_handler;
