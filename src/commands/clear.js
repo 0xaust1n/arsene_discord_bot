211556b0a6da783ca276a96e7a9f4198a2a883d7
Austin <yo861015@gmail.com>
2021-09-15 13:35:36 +0800

    chore: add clear command

---
+++ src/commands/clear.js
@@ -0,0 +1,26 @@
+module.exports = {
+  name: 'clear',
+  description: 'this is a clear command!',
+  aliases: ['cl', 'c'],
+  async execute(msg, args) {
+    if (args[0]) {
+      if (isNaN(args[0])) {
+        return msg.reply(`參數只支援數字 你再瞎雞巴輸入我就幹你`);
+      }
+      if (args[0] > 99 || args[0] == 0) {
+        return msg.reply('請確認數字範圍在 `1` ~ `100` 之間 ');
+      }
+    }
+    const count = parseInt(!args[0] ? 5 : args[0]);
+    await msg.delete(); // delete the command that user input
+    await msg.channel.messages.fetch({ limit: `${count}` }).then((msgs) => {
+      msg.channel.bulkDelete(msgs);
+    });
+
+    await msg.channel.send(`已刪除 \`${!args[0] ? 5 : args[0]}\` 筆訊息`).then(() => {
+      setTimeout(() => {
+        msg.channel.bulkDelete(1);
+      }, 2000);
+    });
+  },
+};
