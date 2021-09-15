c1e487d402045aafe835984c42d6a941802b784f
Austin <yo861015@gmail.com>
2021-09-15 10:54:21 +0800

    chore: update app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
-const fs = require('fs');
-const { Client, Intents, Collection, Discord } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
 const { token } = require('./configs/token.json');
 const myIntents = new Intents(32509);
 const client = new Client({ intents: myIntents });
-//global data
-const prefix = '!';
-const commandList = [];
 //init commands collection from command folder
 client.commands = new Collection();
 client.events = new Collection();
@@ -13,7 +9,7 @@ client.events = new Collection();
 const handlerAry = ['command_handler', 'event_handler'];
 
 handlerAry.forEach((handler) => {
-  require(`./handlers/${handler}`)(client, Discord);
+  require(`./handlers/${handler}`)(client);
 });
 
 client.login(token);
