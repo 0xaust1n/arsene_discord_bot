286333c6b8040816aac3fd6ce6e511973cd45a4a
Austin <yo861015@gmail.com>
2021-09-15 17:24:27 +0800

    chore: delete play command

--- src/commands/play.js
+++
@@ -1,9 +0,0 @@
-const { MessageEmbed } = require('discord.js');
-
-const play = {
-  name: 'play music',
-  description: 'this is a play music command!',
-  execute(msg, args) {},
-};
-
-module.exports = play;
