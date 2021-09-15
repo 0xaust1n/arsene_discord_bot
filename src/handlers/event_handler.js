0b6c54fcdd30d98496e1ae5601d10e237db486e6
Austin <yo861015@gmail.com>
2021-09-15 10:55:03 +0800

    chore: update event handler to support once

--- src/handlers/event_handler.js
+++ src/handlers/event_handler.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 
-const event_handler = (client, Discord) => {
+module.exports = (client) => {
   const load_dir = (dirs) => {
     const eventFiles = fs.readdirSync(`./src/events/${dirs}`).filter((file) => file.endsWith('.js'));
 
     for (const file of eventFiles) {
       const event = require(`../events/client/${file}`);
-      const event_name = file.split('.')[0];
-      client.on(event_name, event.bind(null, Discord, client));
+      if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+      } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+      }
     }
   };
 
   ['client', 'guild'].forEach((e) => load_dir(e));
 };
-
-module.exports = event_handler;
