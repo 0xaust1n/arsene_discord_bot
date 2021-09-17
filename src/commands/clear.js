0d50095c662521fb017fd74fd8edcc0e18529685
Austinbabe <yo861015@gmail.com>
2021-09-17 12:26:44 +0800

    chore: update clear with emoji and permission

--- src/commands/clear.js
+++ src/commands/clear.js
@@ -2,10 +2,12 @@ module.exports = {
   name: 'clear',
   description: 'this is a clear command!',
   aliases: ['cl', 'c'],
-  async execute(msg, args) {
+  prems: ['MANAGE_MESSAGES'],
+  async execute(msg, args, client) {
+    const emoji = client.emojis.cache.get('738680578974548038');
     if (args[0]) {
       if (isNaN(args[0])) {
-        return msg.reply(`參數只支援數字 你再瞎雞巴輸入我就幹你`);
+        return msg.reply(`參數只支援數字 你再瞎雞巴輸入我就幹你 ${emoji}`);
       }
       if (args[0] > 99 || args[0] == 0) {
         return msg.reply('請確認數字範圍在 `1` ~ `100` 之間 ');
