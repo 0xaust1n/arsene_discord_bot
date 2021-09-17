34ce32d90e3e0d10bac42391dfe46eb2c6bc36d7
Austinbabe <yo861015@gmail.com>
2021-09-17 12:26:31 +0800

    chore: update message create with permission handler

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,3 +1,4 @@
+const { Permissions } = require('discord.js');
 const prefix = '!';
 
 module.exports = {
@@ -11,7 +12,21 @@ module.exports = {
     const arg = msg.content.slice(prefix.length).split(/ +/);
     const cmd = arg.shift().toLocaleLowerCase(); //shift first element
     const command = client.commands.get(`${cmd}`) || client.commands.find((a) => a.aliases && a.aliases.includes(cmd));
+    // check command is exist or not
     if (command) {
+      //check permission before execute
+      let isInvalid = false;
+      if (command.prems) {
+        for (const prem of command.prems) {
+          if (!msg.member.permissions.has(Permissions.FLAGS[prem])) {
+            isInvalid = true;
+            break;
+          }
+        }
+      }
+      if (isInvalid) {
+        return msg.channel.send(`你沒有權限使用\`${command.name}\`指令`);
+      }
       command.execute(msg, arg, client);
     }
   },
