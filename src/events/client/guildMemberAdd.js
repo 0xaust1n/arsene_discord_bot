af6a5a3a5cc3cb14ec59199870fad68c5c01ffdf
Austinbabe <yo861015@gmail.com>
2021-09-17 18:51:18 +0800

    fix(bug): update if statement

--- src/events/client/guildMemberAdd.js
+++ src/events/client/guildMemberAdd.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'guildMemberAdd',
   once: false,
   execute(member, client) {
-    if (member.displayName.toLocaleLowerCase().includes('discord') > -1) {
+    if (member.displayName.toLocaleLowerCase().includes('discord')) {
       member.kick(`Bot`);
       console.log(`${member.displayName}被踢出`);
     }
