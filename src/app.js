cf98d29f754afd2a7cc54a949716c573cfa7aec4
Austinbabe <yo861015@gmail.com>
2021-09-18 23:19:35 +0800

    chore: update app.js with init firebase admin

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 const { Client, Intents, Collection } = require('discord.js');
-const { token } = require('./configs/token.json');
+const admin = require('firebase-admin');
 const myIntents = new Intents(32767);
 const client = new Client({ intents: myIntents });
 //init commands collection from command folder
 client.commands = new Collection();
 client.events = new Collection();
-
+//init handler
 const handlerAry = ['command_handler', 'event_handler'];
-
 handlerAry.forEach((handler) => {
   require(`./handlers/${handler}`)(client);
 });
+//init real-time db
+const serviceAccount = require('./configs/serviceAccountKey.json');
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://sodium-burner-326201-default-rtdb.asia-southeast1.firebasedatabase.app',
+});
+
+const { token } = require('./configs/token.json');
 client.login(token);
