652cbc3133a44c15a13f3f48fae097f7a2ef7375
Austinbabe <yo861015@gmail.com>
2021-09-20 02:28:29 +0800

    chore: add chips command

---
+++ src/commands/chips.js
@@ -0,0 +1,24 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'chips',
+  description: 'this is a chips command!',
+  aliases: ['cp'],
+  async execute(msg, args, client) {
+    const leverageUtil = require('../utility/leverage');
+    const emoji = client.emojis.cache.get('889139393376452668');
+    const total = await leverageUtil.get(msg);
+    const resultString = () => {
+      return `
+      **${total}** ${emoji}
+      `;
+    };
+    const resultEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true }))
+      .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+      .addField('籌碼數量為', resultString())
+      .setTimestamp();
+    msg.channel.send({ embeds: [resultEmbed] });
+  },
+};
