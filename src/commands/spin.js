0ee3a1aa0e100c64a38c38cd60323cd1f5113126
Austinbabe <yo861015@gmail.com>
2021-09-20 02:31:30 +0800

    chore: add spin command

---
+++ src/commands/spin.js
@@ -0,0 +1,24 @@
+module.exports = {
+  name: 'spin',
+  description: 'this is a spin command!',
+  aliases: ['sp'],
+  async execute(msg, args, client) {
+    const cd = require('../utility/cool_down');
+    const leverage = require('../utility/leverage');
+    const emoji = client.emojis.cache.get('889139393376452668');
+    cd.get(msg, this.name).then((result) => {
+      if (result == 'READY') {
+        const prize = getRandomInt(50);
+        leverage.add(msg, prize);
+        msg.channel.send(`恭喜你獲得 ${prize} ${emoji}`);
+        cd.set(msg, this.name, 'm', 2);
+      } else {
+        msg.channel.send(`指令冷卻中 剩餘時間:**${result}**`);
+      }
+    });
+  },
+};
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * (max + 1));
+};
