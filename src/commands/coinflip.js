6ab77dcb2b4928c8be2467172f1d6bf03e79f51e
Austinbabe <yo861015@gmail.com>
2021-09-20 02:29:09 +0800

    chore: update coinflip /w leverage

--- src/commands/coinflip.js
+++ src/commands/coinflip.js
@@ -4,30 +4,69 @@ module.exports = {
   name: 'coinflip',
   description: 'this is a coinflip command!',
   aliases: ['cf'],
-  execute(msg, args) {
+  async execute(msg, args, client) {
+    const leverageUtil = require('../utility/leverage');
+    const emoji = client.emojis.cache.get('889139393376452668');
     if (args.length < 1) {
-      msg.channel.send(`擲硬幣指令錯誤! \n範例: \`coinflip tails\``);
+      msg.channel.send(`擲硬幣指令錯誤! \n` + `範例: \`coinflip tails 50\`\n` + `請重新輸入`);
       return;
     }
+    const acceptArgs = ['h', 'heads', 't', 'tails'];
     const input = args.shift().toLocaleLowerCase().substring(0, 1);
-    const acceptArg = ['h', 'heads', 't', 'tails'];
-
-    if (!acceptArg.includes(input)) {
-      msg.channel.send(`擲硬幣指令錯誤! \n範例: \`coinflip tails\``);
+    if (!acceptArgs.includes(input)) {
+      msg.channel.send(`擲硬幣參數錯誤! \n` + `範例: \`coinflip tails\`\n` + `請重新輸入`);
       return;
     }
+    let leverage = 0;
+    const currentLeverage = await leverageUtil.get(msg);
+    const acceptAmountArgs = ['a', 'all'];
+
+    if (isNaN(args[0]) && !acceptAmountArgs.includes(`${args[0].toLocaleLowerCase()}`)) {
+      msg.channel.send(`籌碼數量參數錯誤! 請輸入數字或是 **all** \n` + `範例: \`coinflip tails 50\`\n` + `請重新輸入`);
+      return;
+    }
+
+    if (parseInt(args[0]) <= 0) {
+      msg.channel.send(`籌碼數量參數錯誤! 數量請大於0 \n` + `範例: \`coinflip tails 50\`\n` + `請重新輸入`);
+      return;
+    }
+
+    if (currentLeverage == 0) {
+      msg.channel.send(`籌碼數量錯誤! 籌碼為 0 \n` + `範例: \`coinflip tails 50\`\n` + `請重新輸入`);
+      return;
+    }
+
+    if (acceptAmountArgs.includes(`${args[0].toLocaleLowerCase()}`)) {
+      leverage = currentLeverage;
+    } else {
+      leverage = Math.floor(parseInt(args.shift()));
+    }
+
+    const isEnough = currentLeverage >= leverage ? true : false;
+    if (!isEnough) {
+      msg.channel.send(
+        `籌碼數量錯誤! 沒有足夠的籌碼 \n` +
+          `目前籌碼數量為 ${currentLeverage} ${emoji}\n` +
+          `範例: \`coinflip tails 50\`` +
+          `請重新輸入`
+      );
+      return;
+    }
+
     //random flip result
-    const flip = acceptArg[getRandomInt(4)].substring(0, 1);
+    const flip = acceptArgs[getRandomInt(4)].substring(0, 1);
     const result = input == flip ? true : false;
+    const gaining = result == true ? leverage : -leverage;
     const coinImgMap = new Map();
+    const total = await leverageUtil.add(msg, gaining);
     coinImgMap.set('h', 'https://i.imgur.com/aO5MiC8.png');
     coinImgMap.set('t', 'https://i.imgur.com/qt6tqBM.png');
     const resultString = () => {
-      return `
-      你猜的是${input == 'h' ? '正面' : '反面'}! 
-      結果是${flip == 'h' ? '正面' : '反面'}!
-      你${result ? '淫了' : '輸了'}!
-      `;
+      return (
+        `結果為**${flip == 'h' ? '正面' : '反面'}** \n` +
+        `你${result ? '贏得了' : '損失了'} ${Math.abs(gaining)} ${emoji} \n` +
+        `現在籌碼數量為${total} ${emoji} \n`
+      );
     };
     // prettier-ignore
     const resultEmbed = new MessageEmbed()
