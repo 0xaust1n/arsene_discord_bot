183388454f3fd2beaf914aff75fc049a30207128
Austinbabe <yo861015@gmail.com>
2021-09-20 18:00:25 +0800

    fix(bug): amount undefined handling

--- src/utility/leverage.js
+++ src/utility/leverage.js
@@ -9,9 +9,13 @@ module.exports = {
     const amount = parseInt(a);
     const dbResult = await ref.child(msg.author.id).once('value');
     const temp = dbResult.val();
+    if (temp.amount == undefined) {
+      temp.amount = 50;
+    }
     if (temp != null) {
       let currentAmount = parseInt(temp.amount) + amount;
       ref.child(`${msg.author.id}`).update({
+        name: `${msg.author.username}`,
         amount: currentAmount,
       });
       return currentAmount;
@@ -25,7 +29,6 @@ module.exports = {
       return currentAmount;
     } else {
       ref.child(`${msg.author.id}`).set({
-        id: `${msg.author.id}`,
         name: `${msg.author.username}`,
         amount: `${50}`,
       });
