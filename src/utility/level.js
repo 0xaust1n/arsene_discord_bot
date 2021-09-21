c10c733bf0c705c159d5869f41e43b632c84ae15
Austinbabe <yo861015@gmail.com>
2021-09-21 23:13:42 +0800

    chore: add level utility

---
+++ src/utility/level.js
@@ -0,0 +1,53 @@
+const admin = require('firebase-admin');
+const db = admin.database();
+const ref = db.ref(`/leverage`);
+
+module.exports = {
+  name: 'level',
+  description: 'utility for function level',
+  checkLevel: (bExp, AExp) => {
+    const levelAry = [1000, 10000, 100000, 500000, 1000000];
+    const temp = {};
+    for (const val of levelAry) {
+      if (bExp < val && AExp >= val) {
+        temp.isLevelUp = true;
+        temp.level = val;
+        break;
+      } else {
+        continue;
+      }
+    }
+    return temp;
+  },
+  levelUp: async (msg, level) => {
+    const levelObject = {
+      0: 'Fish',
+      1000: 'Newbie',
+      10000: 'Gambler',
+      100000: 'Poker Facer',
+      500000: 'Rich Fucker',
+      1000000: 'Hand Of Fate',
+    };
+    const nextLevel = levelObject[level];
+    ref.child(`${msg.author.id}`).update({
+      name: `${msg.author.username}`,
+      level: `${nextLevel}`,
+    });
+    return msg.channel.send(`恭喜 \`${msg.author.username}\`  賭博等級升級為 \`${nextLevel}\``);
+  },
+  get: async (msg) => {
+    const dbResult = await ref.child(msg.author.id).once('value');
+    const temp = dbResult.val();
+    return temp.level;
+  },
+  getNextLevelXp: (xp) => {
+    const levelAry = [1000, 10000, 100000, 500000, 1000000];
+    for (const val of levelAry) {
+      if (xp <= val) {
+        return val;
+      } else {
+        continue;
+      }
+    }
+  },
+};
