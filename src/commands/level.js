b429f0ac9e8e1ca68c248b0a4838cf375fb53be2
Austinbabe <yo861015@gmail.com>
2021-09-21 23:14:01 +0800

    chore: add level command

---
+++ src/commands/level.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'level',
+  description: 'this is a level command!',
+  aliases: ['lv'],
+  async execute(msg, args, client) {
+    const level = require('../utility/level');
+    const xp = require('../utility/xp');
+    const leverage = require('../utility/leverage');
+    const currentLeverage = await leverage.get(msg);
+    const currentLevel = await level.get(msg);
+    const currentXp = await xp.get(msg);
+    const nextXp = level.getNextLevelXp(currentXp);
+    let sectionOne = `\`等級\`: ${currentLevel}\n`;
+    sectionOne += `\`經驗值\`: ${currentXp}\n`;
+    sectionOne += `\`距離升級\`: ${parseInt(nextXp) - parseInt(currentXp)}\n`;
+    const resultEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true }))
+      .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
+      .addField('Level', sectionOne)
+      .setTimestamp();
+
+    msg.channel.send({ embeds: [resultEmbed] });
+  },
+};
