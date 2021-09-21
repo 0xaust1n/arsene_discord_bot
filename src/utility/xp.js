5e594b52858b151a778d5afea93e17f490c20aeb
Austinbabe <yo861015@gmail.com>
2021-09-21 23:13:50 +0800

    chore: add xp utility

---
+++ src/utility/xp.js
@@ -0,0 +1,42 @@
+const admin = require('firebase-admin');
+const db = admin.database();
+const ref = db.ref(`/leverage`);
+const level = require('../utility/level');
+
+module.exports = {
+  name: 'xp',
+  description: 'utility for function xp',
+  add: async (msg, exp) => {
+    const expAmount = parseInt(exp);
+    const dbResult = await ref.child(msg.author.id).once('value');
+    const temp = dbResult.val();
+    if (temp.xp == undefined) {
+      temp.xp = 0;
+    }
+    if (temp != null) {
+      let currentXp = parseInt(temp.xp) + expAmount;
+      ref.child(`${msg.author.id}`).update({
+        name: `${msg.author.username}`,
+        xp: currentXp,
+      });
+      const checkLevel = level.checkLevel(temp.xp, currentXp);
+      if (checkLevel.isLevelUp) {
+        level.levelUp(msg, checkLevel.level);
+      }
+    }
+  },
+  get: async (msg) => {
+    const dbResult = await ref.child(msg.author.id).once('value');
+    const temp = dbResult.val();
+    if (temp != null && temp.xp != undefined) {
+      let currentXP = parseInt(temp.xp);
+      return currentXP;
+    } else {
+      ref.child(`${msg.author.id}`).set({
+        name: `${msg.author.username}`,
+        xp: `${0}`,
+      });
+      return 0;
+    }
+  },
+};
