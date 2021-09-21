e207cf3ac532388670424b487eb907b6c8b4ad95
Austinbabe <yo861015@gmail.com>
2021-09-21 23:13:05 +0800

    chore: update leverage utility

--- src/utility/leverage.js
+++ src/utility/leverage.js
@@ -9,9 +9,6 @@ module.exports = {
     const amount = parseInt(a);
     const dbResult = await ref.child(msg.author.id).once('value');
     const temp = dbResult.val();
-    if (temp.amount == undefined) {
-      temp.amount = 50;
-    }
     if (temp != null) {
       let currentAmount = parseInt(temp.amount) + amount;
       ref.child(`${msg.author.id}`).update({
@@ -31,6 +28,8 @@ module.exports = {
       ref.child(`${msg.author.id}`).set({
         name: `${msg.author.username}`,
         amount: `${50}`,
+        xp: 0,
+        level: `Fish`,
       });
       return 50;
     }
