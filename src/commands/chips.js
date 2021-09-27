611be8e452485e5fe3c7c47f98b07e33c9b06fb9
Austin <yo861015@gmail.com>
2021-09-27 10:01:29 +0800

    chore: update chips

--- src/commands/chips.js
+++ src/commands/chips.js
@@ -5,32 +5,28 @@ module.exports = {
   description: 'this is a chips command!',
   aliases: ['cp'],
   async execute(msg, args, client) {
-    const leverageUtil = require('../utility/leverage');
+    const leverage = require('../utility/leverage');
     const emoji = client.emojis.cache.get('889219097752129667');
-    const total = await leverageUtil.get(msg);
+    const total = await leverage.get(msg);
     const resultString = () => {
       return `
       **${total}** ${emoji}
       `;
     };
-
-    let resultEmbed;
+    let user;
     if (msg.mentions.users.first() != undefined) {
-      const metions = msg.mentions.users.first();
-      resultEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setAuthor(metions.username, metions.displayAvatarURL({ dynamic: true }))
-        .setThumbnail(metions.displayAvatarURL({ dynamic: true }))
-        .addField('籌碼數量', resultString())
-        .setTimestamp();
+      user = msg.mentions.users.first();
     } else {
-      resultEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true }))
-        .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-        .addField('籌碼數量', resultString())
-        .setTimestamp();
+      user = msg.author;
     }
+
+    const resultEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(user.username, user.displayAvatarURL({ dynamic: true }))
+      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+      .addField('籌碼數量', resultString())
+      .setTimestamp();
+
     msg.channel.send({ embeds: [resultEmbed] });
   },
 };
