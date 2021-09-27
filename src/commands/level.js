2a100197d986434fadc4001e2ecd43c955be3c6f
Austin <yo861015@gmail.com>
2021-09-27 17:40:10 +0800

    chore: update level command

--- src/commands/level.js
+++ src/commands/level.js
@@ -8,30 +8,25 @@ module.exports = {
     const level = require('../utility/level');
     const xp = require('../utility/xp');
     const leverage = require('../utility/leverage');
-    const currentLeverage = await leverage.get(msg);
-    const currentLevel = await level.get(msg);
-    const currentXp = await xp.get(msg);
+    let user;
+    if (msg.mentions.users.first() != undefined) {
+      user = msg.mentions.users.first();
+    } else {
+      user = msg.author;
+    }
+    const currentLeverage = await leverage.get(user);
+    const currentLevel = await level.get(user);
+    const currentXp = await xp.get(user);
     const nextXp = level.getNextLevelXp(currentXp);
     let sectionOne = `\`等級\`: ${currentLevel}\n`;
     sectionOne += `\`經驗值\`: ${currentXp}\n`;
     sectionOne += `\`距離升級\`: ${parseInt(nextXp) - parseInt(currentXp)}\n`;
-    let resultEmbed;
-    if (msg.mentions.users.first() != undefined) {
-      const metions = msg.mentions.users.first();
-      resultEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setAuthor(metions.username, metions.displayAvatarURL({ dynamic: true }))
-        .setThumbnail(metions.displayAvatarURL({ dynamic: true }))
-        .addField('Level', sectionOne)
-        .setTimestamp();
-    } else {
-      resultEmbed = new MessageEmbed()
-        .setColor('#0099ff')
-        .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true }))
-        .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-        .addField('Level', sectionOne)
-        .setTimestamp();
-    }
+    const resultEmbed = new MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(user.username, user.displayAvatarURL({ dynamic: true }))
+      .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+      .addField('Level', sectionOne)
+      .setTimestamp();
 
     msg.channel.send({ embeds: [resultEmbed] });
   },
