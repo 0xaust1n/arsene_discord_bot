8ed922fd33d4daa76f07fa196d956315fba39f98
Austin <yo861015@gmail.com>
2021-09-27 17:37:43 +0800

    chore: update leverage

--- src/utility/leverage.js
+++ src/utility/leverage.js
@@ -4,27 +4,21 @@ const ref = db.ref(`/leverage`);
 module.exports = {
   name: 'leverage ',
   description: 'utility for leverage',
-  add: async (msg, a) => {
+  add: async (user, a) => {
     //return int
     const amount = parseInt(a);
-    const dbResult = await ref.child(msg.author.id).once('value');
+    const dbResult = await ref.child(user.id).once('value');
     const temp = dbResult.val();
     if (temp != null) {
       let currentAmount = parseInt(temp.amount) + amount;
-      ref.child(`${msg.author.id}`).update({
-        name: `${msg.author.username}`,
+      ref.child(`${user.id}`).update({
+        name: `${user.username}`,
         amount: currentAmount,
       });
       return currentAmount;
     }
   },
-  get: async (msg) => {
-    let user;
-    if (msg.mentions.users.first() != undefined) {
-      user = msg.mentions.users.first();
-    } else {
-      user = msg.author;
-    }
+  get: async (user) => {
     dbResult = await ref.child(user.id).once('value');
     const temp = dbResult.val();
     if (temp != null) {
