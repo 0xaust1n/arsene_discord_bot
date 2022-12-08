017496296d27a277357b9888ae71be5ef44f81a6
0xaust1n <yo861015@gmail.com>
2022-12-08 11:55:23 +0800

    fix: embed

--- src/commands/cooldown.js
+++ src/commands/cooldown.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: 'cooldown',
@@ -7,6 +7,7 @@ module.exports = {
   async execute(msg, args, client) {
     const cd = require('../utility/cd');
     const cdKeyAry = ['jackpot', 'hourly', 'daily', 'weekly'];
+    const user = msg.author;
     const cdResultAry = [];
     for (const key of cdKeyAry) {
       const temp = await cd.get(msg, key);
@@ -16,11 +17,11 @@ module.exports = {
     for (let i = 0; i < cdResultAry.length; i++) {
       resultString += `\`${cdKeyAry[i]}\` : ${cdResultAry[i]}\n`;
     }
-    const resultEmbed = new MessageEmbed()
+    const resultEmbed = new EmbedBuilder()
       .setColor('#0099ff')
-      .setAuthor(msg.author.username, msg.author.displayAvatarURL({ dynamic: true }))
+      .setAuthor({ name: user.username, iconURL: user.displayAvatarURL({ dynamic: true }) })
       .setThumbnail(msg.author.displayAvatarURL({ dynamic: true }))
-      .addField('冷卻時間', resultString)
+      .addFields({ name: '冷卻時間', value: resultString })
       .setTimestamp();
     msg.channel.send({ embeds: [resultEmbed] });
   },
