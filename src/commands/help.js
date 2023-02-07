480145622b70824c9d17f37204d8b973c352729b
0xaust1n <yo861015@gmail.com>
2023-02-07 15:25:23 +0800

    chore(help): update

--- src/commands/help.js
+++ src/commands/help.js
@@ -21,22 +21,28 @@ module.exports = {
     const gambleCommand = () => {
       return (
         `\`level\` , \`chips\` , \`jackpot\` , \`claimall\` \n` +
-        `\`cooldown\` , \`reward\`  , \`coinflip\` , \`beg\`\n`
+        `\`cooldown\` , \`reward\`  , \`coinflip\` , \`beg\`\n` +
+        `\`give\` , \`pk\` `
       );
+    };
+
+    const bankCommand = () => {
+      return `\`atm\``;
     };
     const resultEmbed = new EmbedBuilder()
       .setColor('#0099ff')
       .setAuthor({
         name: '→詳細指令解釋點我',
         iconURL: client.user.displayAvatarURL({ dynamic: true }),
-        url: 'https://github.com/austinbabe/arsene_discord_bot/blob/master/command_cht.md',
+        url: 'https://github.com/0xaust1n/arsene_discord_bot/blob/master/command_list.cht.md',
       })
       .setDescription(`目前指令前綴為: \`${prefix}\``)
       .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
       .addFields(
         { name: '一般指令:', value: generalCommand() },
         { name: '管理員指令:', value: adminCommands() },
-        { name: '賭博指令:', value: gambleCommand() }
+        { name: '賭博指令:', value: gambleCommand() },
+        { name: '銀行指令:', value: bankCommand() }
       )
       .setTimestamp();
 
