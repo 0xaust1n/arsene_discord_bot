5948e93af6aae717936b2373112326137860ae1c
0xaust1n <yo861015@gmail.com>
2024-09-21 23:06:39 +0800

    chore(event): update listener for button interaction

--- src/events/client/interactionCreate.js
+++ src/events/client/interactionCreate.js
@@ -2,20 +2,34 @@ module.exports = {
   name: 'interactionCreate',
   once: false,
   async execute(interaction, client) {
-    if (!interaction.isChatInputCommand()) return;
+    if (interaction.isChatInputCommand()) {
+      const command = interaction.client.commands.get(interaction.commandName);
 
-    const command = interaction.client.commands.get(interaction.commandName);
+      if (!command) {
+        console.error(`No command matching ${interaction.commandName} was found.`);
+        return;
+      }
 
-    if (!command) {
-      console.error(`No command matching ${interaction.commandName} was found.`);
-      return;
-    }
+      try {
+        await command.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
+    } else if (interaction.isButton()) {
+      const buttonAction = client.commands.get(interaction.customId);
 
-    try {
-      await command.execute(interaction);
-    } catch (error) {
-      console.error(error);
-      await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      if (!buttonAction) {
+        console.error(`No button matching ${interaction.customId} was found.`);
+        return;
+      }
+
+      try {
+        await buttonAction.execute(interaction);
+      } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
     }
   },
 };
