586014f3312c7fb49a5c0d38b01945e697bfeeaf
0xaust1n <yo861015@gmail.com>
2025-05-05 14:42:35 +0800

    fix(gm-give): args detect

--- src/commands/gm.js
+++ src/commands/gm.js
@@ -7,6 +7,16 @@ module.exports = {
   async execute(msg, args, client) {
     const leverage = require('../utility/leverage');
     const emoji = client.emojis.cache.get('889219097752129667');
+    const accpetArgs = ['on', 'off', 'give', 'g'];
+
+    if (args[0] && !accpetArgs.includes(args[0])) {
+      msg.reply({
+        content: `請輸入正確的指令`,
+        ephemeral: true,
+      });
+      return;
+    }
+
     if (args[0] == 'on') {
       await leverage.set(msg.author, 999999999999);
       msg.reply({
@@ -35,12 +45,16 @@ module.exports = {
       const resultEmbed = new EmbedBuilder()
         .setColor('#0099ff')
         .setAuthor({
-          name: msg.author.username,
-          iconURL: msg.author.displayAvatarURL({ dynamic: true }),
+          name: 'GM',
+          iconURL: client.user.displayAvatarURL({ dynamic: true }),
         })
+        .setThumbnail(client.user.displayAvatarURL({ dynamic: true }))
         .addFields({
-          name: `GM空投`,
-          value: `GM抖內了 \`${amount}\` ${emoji} \n` + `給你  <@${target.id}>`,
+          name: `GM抖內警報`,
+          value:
+            `GM抖內給 <@${target.id}> \n` +
+            `金額: ${amount} ${emoji} \n` +
+            `餘額: ${await leverage.get(target)} ${emoji}`,
         })
         .setTimestamp();
 
