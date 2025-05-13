37d3ff760db44bb2227ceb28a3e6530abe799b6b
0xaust1n <yo861015@gmail.com>
2025-05-13 14:50:21 +0800

    chore: display local string number

--- src/commands/chips.js
+++ src/commands/chips.js
@@ -17,13 +17,16 @@ module.exports = {
     const total = await leverage.get(user);
     const resultString = () => {
       return `
-      **${total}** ${emoji}
+      **${total.toLocaleString()}** ${emoji}
       `;
     };
 
     const resultEmbed = new EmbedBuilder()
       .setColor(0x0099ff)
-      .setAuthor({ name: user.username, iconURL: user.displayAvatarURL({ dynamic: true }) })
+      .setAuthor({
+        name: user.username,
+        iconURL: user.displayAvatarURL({ dynamic: true }),
+      })
       .setThumbnail(user.displayAvatarURL({ dynamic: true }))
       .addFields({ name: '籌碼數量', value: resultString() })
       .setTimestamp();
